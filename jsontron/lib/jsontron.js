111ab86c2abd6c742167aa8e5a67793da018d5ce
DPS <DPS@DESKTOP-C9IJTD4.fios-router.home>
2017-01-15 15:03:52 -0500

    improved assert test with jpq:xxx format

--- jsontron/lib/jsontron.js
+++ jsontron/lib/jsontron.js
@@ -14,14 +14,63 @@ console.log(names);
 
 var jp = require('jsonpath');
 
-var schTRON = require("../data/schematron-instance-minimal-complex.json");
-console.log(schTRON);
+//var schTRON = require("../data/schematron-instance-minimal-complex.json");
+var schInstance = require("../data/loandata-instance-simple.json");
+var myRule = require("../data/loandata-instance-single-rule.json");
 
-var phases = jp.query(schTRON, '$..phase[1].active');
-var pattern2 = jp.query(schTRON, '$..pattern');
-
-console.log(phases);
-console.log(pattern2);
+console.log(schInstance);
+console.log(myRule);
 
 
+var loanid = jp.query(schInstance, '$.loan_data.loans[0].loan_id');
+//var result = eval("loanid > 1 && loanid < 2234567");
+//var phases = jp.query(schTRON, '$..phase[1].active');
+//var pattern2 = jp.query(schTRON, '$..pattern');
 
+var loanid2 = jp.query(schInstance, '$.loan_data.loans[0].loan_id < 1)');
+
+
+console.log("LoanID: "+ loanid);
+console.log("LoanID2: "+ loanid2);
+//console.log("Result: "+ result);
+console.log(loanid.length);
+//console.log(pattern2);
+
+
+var myAssert = {
+        "id":"assert id 1",
+        "test": "loanid < 1 && loanid < 2234567",
+        "message": "Assert 1: Loan ID cannot be less than 1 or greater than 2234567"
+     };
+
+var parseAssert = function (assert){
+	
+	let result = eval(assert.test); // TODO: do strict eval for security reasons
+	
+	return {"id":assert.id, "result":result, "message":assert.message};
+	
+}
+
+var parsedAssert = parseAssert(myAssert);
+
+
+var validateAssert = function (parsedAssert){
+	let msg = parsedAssert.result ? "successful" : parsedAssert.message;
+	console.log ("Test: "+ parsedAssert.result);
+	console.log ("Message: " + msg);
+
+
+}
+
+validateAssert(parsedAssert);
+
+var  parseRule = function(instance, rule){
+	
+	// first parse the context
+	
+	parsedContext = jp.query(instance, rule.context)
+	console.log(parsedContext);
+	
+}
+
+parseRule(schInstance, myRule);
