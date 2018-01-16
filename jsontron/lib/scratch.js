b9fb4c5b5f67eef8e5f32f32ef732ef8a3f1759b
DPS <DPS@DESKTOP-C9IJTD4>
2018-01-15 23:32:08 -0500

    IBM Tests 3.7 to 4.5

--- jsontron/lib/scratch.js
+++ jsontron/lib/scratch.js
@@ -68,15 +68,37 @@ var testErrorReport = function(arg1, arg2){
 //var schInstance1 = require("../data/loandata-instance-simple.json");
 //var mySchRules1 = require("../data/loandata-instance-single-schematest2.json");
 var jp = require('jsonpath');
-var schInstance1 = require("../data/ibm-test-suite/3.6/eg3_6_bad1.json");
-var mySchRules1 = require("../data/ibm-test-suite/3.6/eg3_6-rules.json");
+var schInstance1 = require("../data/ibm-test-suite/4.5/eg4_5_bad1.json");
+var mySchRules1 = require("../data/ibm-test-suite/4.5/eg4_5-rules.json");
 var jsontron = require('./jsontron.js');
 
-ruleParsedContext = jp.query(schInstance1, "$..prologue.keyword");
-console.log(ruleParsedContext);
+contextNode = jp.query(schInstance1, "$..doc");
+console.log(contextNode);
 
-console.log(ruleParsedContext.length);
-console.log(ruleParsedContext[0].length);
+//contextNodeN = jp.nodes(schInstance1, "$..[?(@.prologue)]");
+//console.log(contextNodeN);
+
+
+//military = (jp.query(contextNode, '$..[?(@.email=="okey.agu.navy.mil")]'));
+link = (jp.query(contextNode, '$..[?(@.prologue)]')).length > 0 ;
+console.log(link);
+//console.log(contextNode[0].length);
+//allowedChild = contextNode[0] in ['member', 'email','name','bio','affiliation'];
+//console.log(allowedChild);
+//console.log(jp.query(contextNode, '$..[?(@.name || @.bios || @.affiliations)]'));
+//console.log(jp.query(contextNode, '$.*[?(@.name || @.bio || @.affiliation)]'));
+//children = jp.query(contextNode, '$.*[?(@.length)]');
+//children = contextNode.length == 1 && jp.query(contextNode, '$..name[0]') !=null && jp.query(contextNode, '$..bio[0]') !=null && jp.query(contextNode, '$..affiliation[0]') !=null ;
+//console.log(children);
+
+//console.log(children.length);
+//email = (jp.query(ruleParsedContext, "$..email")).length > 0;
+//console.log(email);
+//member = (jp.query(ruleParsedContext, "$..member")).length >0 && (jp.query(ruleParsedContext, "$..member"))[0]== 'yes';
+//member = (jp.query(ruleParsedContext, "$..member"))[0].length > 0 && ((jp.query(ruleParsedContext, "$..member"))[0] == ('yes')||(jp.query(ruleParsedContext, "$..member"))[0] == ('no'));
+//member = ('member' in ruleParsedContext[0])&& ((jp.query(ruleParsedContext, "$..member"))[0] == ('yes')||(jp.query(ruleParsedContext, "$..member"))[0] == ('no'));
+//console.log(('member' in ruleParsedContext[0]))
+//console.log(member);
 //console.log(jp.parent(ruleParsedContext,'$..doc'));
 //console.log(jp.query(ruleParsedContext, '$..doc').length);
 
