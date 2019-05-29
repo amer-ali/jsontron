53b3bd38f6d8474fa073979d8ae50d0578c56a9d
Stefan Hoops <shoops@copasi.org>
2019-05-29 08:51:04 -0400

    ESLint suggested changes.

--- jsontron/bin/querybuilder.js
+++ jsontron/bin/querybuilder.js
@@ -1,21 +1,21 @@
+/* eslint-disable max-len */
 /**
  * This utility can be used to build and test the 'context' and assertion 'test' expressions
  * Just point schInstance to your instance and then build the contextNode
  * Then use the contextNode to build the test expression
  */
-var jp = require('jsonpath');
+const jp = require('jsonpath');
 
-var schInstance = require("../data/dissertation/context/bookdata_dataForContext_good1.json");
-var schInstance = require("../data/dissertation/context/loandata_dataForContext_good1.json");
-//var authors = jp.query(schInstance, '$.store.book');
+const schInstance = require('../data/dissertation/context/bookdata_dataForContext_good1.json');
+// const schInstance = require('../data/dissertation/context/loandata_dataForContext_good1.json');
+// var authors = jp.query(schInstance, '$.store.book');
 
-//var schInstance = require("./data/dissertation/loandata-main/loandata-main-bad1.json");
-//var loans = jp.query(schInstance, "$..loan_data.loans[?(@.origination_id == 'branch')]");
+// const schInstance = require("./data/dissertation/loandata-main/loandata-main-bad1.json");
+// var loans = jp.query(schInstance, "$..loan_data.loans[?(@.origination_id == 'branch')]");
 
 
-
-var contextNode = jp.query(schInstance, "$..loans.*"); // select all book items in the document
-//var contextNode = jp.query(schInstance, "$.loan_data.loans[?(@.loan_type === 'FHA')]");
+const contextNode = jp.query(schInstance, '$..loans.*'); // select all book items in the document
+// var contextNode = jp.query(schInstance, "$.loan_data.loans[?(@.loan_type === 'FHA')]");
 console.log(contextNode);
 console.log(contextNode.length);
 
@@ -23,11 +23,9 @@ console.log(contextNode.length);
 // it selects the books that are priced more than 20 dollars
 // one crucial difference to note between this utility and the jsontron implementation
 // here the test will not run iteratively against all the nodes returned from contextNode expression
-var test = jp.query(contextNode, "$..[?(@.loan_type == 'FHA')]");
+const test = jp.query(contextNode, '$..[?(@.loan_type == \'FHA\')]');
 
 console.log(test);
-console.log(test.length)
-
-
+console.log(test.length);
 
 
